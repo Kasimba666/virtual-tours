c9a35c35a8c1c4bc332229e463c447a3ba49caea
Kasimba666 <Kasimba666@gmail.com>
2026-01-30 18:33:42 +0300

    Корректная загрузка панорам в обе вкладки

--- src/pages/SceneEditorPage.vue
+++ src/pages/SceneEditorPage.vue
@@ -16,19 +16,6 @@
         </el-form-item>
 
         <el-tabs v-model="activeTab" type="border-card" @tab-click="onTabClick">
-          <el-tab-pane label="Хотспоты" name="hotspots">
-            <div class="hotspots-viewer">
-              <panorama-viewer
-                  v-if="form.panorama"
-                  ref="hotspotsViewer"
-                  :src="form.panorama"
-                  @ready="onHotspotsViewerReady"
-                  @camera-move="onCameraMove"
-                  class="hotspots-viewer__viewer"
-              />
-            </div>
-          </el-tab-pane>
-
           <el-tab-pane label="Настройки вида" name="viewer">
             <el-form-item label="Панорама">
               <el-upload
@@ -135,13 +122,26 @@
             <el-divider />
 
             <panorama-viewer
-                v-if="form.panorama && viewerInitialized"
+                v-if="form.panorama"
                 ref="viewer"
                 :src="form.panorama"
                 @ready="onViewerReady"
                 @camera-move="onCameraMove"
                 class="scene-editor-page__viewer"
             />
+          </el-tab-pane>
+
+          <el-tab-pane label="Хотспоты" name="hotspots">
+            <div class="hotspots-viewer">
+              <panorama-viewer
+                  v-if="form.panorama && hotspotsViewerInitialized"
+                  ref="hotspotsViewer"
+                  :src="form.panorama"
+                  @ready="onHotspotsViewerReady"
+                  @camera-move="onCameraMove"
+                  class="hotspots-viewer__viewer"
+              />
+            </div>
           </el-tab-pane>
         </el-tabs>
 
@@ -179,8 +179,11 @@ export default {
       loadingFromDatabase: false, // флаг загрузки из базы данных
       applyingStartView: false,   // флаг применения параметров камеры
       activeCollapse: [],         // активные сворачиваемые панели (по умолчанию свернуты)
-      activeTab: 'hotspots',      // активная вкладка (по умолчанию "Хотспоты")
+      activeTab: 'viewer',        // активная вкладка (по умолчанию "Настройки вида")
       viewerInitialized: false,   // флаг инициализации панорамы во вкладке Настройки вида
+      hotspotsViewerInitialized: false, // флаг инициализации панорамы во вкладке Хотспоты
+      viewerCameraView: null,     // текущие параметры камеры во вкладке Настройки вида
+      hotspotsCameraView: null,   // текущие параметры камеры во вкладке Хотспоты
       form: {
         name: '',
         panorama: '',
@@ -250,11 +253,28 @@ export default {
       // Обновляем параметры камеры в реальном времени при движении
       // Но не меняем их, если мы только что загрузили сцену или применяем параметры
       if (!this.loadingFromDatabase && !this.applyingStartView) {
-        // Создаем новый объект, чтобы избежать проблем с реактивностью
-        this.form.startView = {
-          yaw: view.yaw,
-          pitch: view.pitch,
-          fov: view.fov
+        // Определяем, из какой вкладки пришло событие
+        const isViewerTab = this.activeTab === 'viewer'
+        
+        if (isViewerTab) {
+          // Обновляем параметры для вкладки Настройки вида
+          this.viewerCameraView = {
+            yaw: view.yaw,
+            pitch: view.pitch,
+            fov: view.fov
+          }
+          
+          // Обновляем значения инпутов в форме
+          this.form.startView.yaw = view.yaw
+          this.form.startView.pitch = view.pitch
+          this.form.startView.fov = view.fov
+        } else {
+          // Обновляем параметры для вкладки Хотспоты
+          this.hotspotsCameraView = {
+            yaw: view.yaw,
+            pitch: view.pitch,
+            fov: view.fov
+          }
         }
       }
     },
@@ -441,10 +461,72 @@ export default {
           const viewer = this.$refs.viewer
           if (viewer && this.form.panorama) {
             console.log('Применяем начальные параметры к панораме во вкладке Настройки вида...')
-            viewer.setCameraView(this.form.startView)
+            
+            // Получаем оригинальные параметры из базы данных напрямую
+            const tour = this.tour
+            const scene = tour.data.scenes[this.sceneId]
+            const originalStartView = scene.startView || {
+              yaw: 0,
+              pitch: 0,
+              fov: 60
+            }
+            
+            console.log('Оригинальные параметры камеры из базы данных:', originalStartView)
+            
+            // Создаем новый объект, чтобы избежать проблем с реактивности
+            const cameraView = {
+              yaw: originalStartView.yaw,
+              pitch: originalStartView.pitch,
+              fov: originalStartView.fov
+            }
+            
+            viewer.setCameraView(cameraView)
             viewer.setBrightness(this.form.effects.brightness)
             viewer.setContrast(this.form.effects.contrast)
             viewer.setSaturation(this.form.effects.saturation)
+          }
+        })
+      }
+      
+      // Если перешли во вкладку "Хотспоты" и панорама еще не инициализирована
+      if (tab.props.name === 'hotspots' && this.form.panorama && !this.hotspotsViewerInitialized) {
+        console.log('Инициализация панорамы во вкладке Хотспоты')
+        this.hotspotsViewerInitialized = true
+        
+        // Применяем начальные параметры к панораме во вкладке Хотспоты
+        this.$nextTick(() => {
+          const viewer = this.$refs.hotspotsViewer
+          if (viewer && this.form.panorama) {
+            console.log('Применяем начальные параметры к панораме во вкладке Хотспоты...')
+            
+            // Получаем оригинальные параметры из базы данных напрямую
+            const tour = this.tour
+            const scene = tour.data.scenes[this.sceneId]
+            const originalStartView = scene.startView || {
+              yaw: 0,
+              pitch: 0,
+              fov: 60
+            }
+            
+            console.log('Оригинальные параметры камеры из базы данных:', originalStartView)
+            
+            // Создаем новый объект, чтобы избежать проблем с реактивности
+            const cameraView = {
+              yaw: originalStartView.yaw,
+              pitch: originalStartView.pitch,
+              fov: originalStartView.fov
+            }
+            
+            // Применяем параметры с небольшой задержкой для надежности
+            setTimeout(() => {
+              viewer.setCameraView(cameraView)
+              viewer.setBrightness(this.form.effects.brightness)
+              viewer.setContrast(this.form.effects.contrast)
+              viewer.setSaturation(this.form.effects.saturation)
+              
+              // Сохраняем эти параметры как начальные для вкладки Хотспоты
+              this.hotspotsCameraView = cameraView
+            }, 100)
           }
         })
       }
@@ -463,8 +545,36 @@ export default {
           viewer.setContrast(this.form.effects.contrast)
           viewer.setSaturation(this.form.effects.saturation)
           
-          console.log('Применяем начальные параметры камеры к панораме в вкладке Хотспоты...')
-          viewer.setCameraView(this.form.startView)
+          // Применяем начальные параметры камеры из базы данных при первом открытии
+          if (!this.hotspotsCameraView) {
+            console.log('Применяем начальные параметры камеры из базы данных к панораме в вкладке Хотспоты...')
+            
+            // Получаем оригинальные параметры из базы данных напрямую
+            const tour = this.tour
+            const scene = tour.data.scenes[this.sceneId]
+            const originalStartView = scene.startView || {
+              yaw: 0,
+              pitch: 0,
+              fov: 60
+            }
+            
+            console.log('Оригинальные параметры камеры из базы данных:', originalStartView)
+            
+            // Создаем новый объект, чтобы избежать проблем с реактивностью
+            const cameraView = {
+              yaw: originalStartView.yaw,
+              pitch: originalStartView.pitch,
+              fov: originalStartView.fov
+            }
+            
+            viewer.setCameraView(cameraView)
+            
+            // Сохраняем эти параметры как начальные для вкладки Хотспоты
+            this.hotspotsCameraView = cameraView
+          } else {
+            console.log('Применяем текущие параметры камеры для вкладки Хотспоты...')
+            viewer.setCameraView(this.hotspotsCameraView)
+          }
           
           // Повторно применяем эффекты через еще одну небольшую задержку
           setTimeout(() => {
